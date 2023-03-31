ccc80f46b5ddc7eacf427e7c352223f3dcf3ab90
zengks <stevevitasks@hotmail.com>
2023-03-30 23:09:47 -0700

    first verison of markdown previewer completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { marked } from 'marked'
+
+import styles from './styles.module.css'
+import { useEffect, useState } from 'react'
+
+import raw from './marked.txt'
 
 function App() {
+  const [markedDown, setMarkedDown] = useState('')
+
+  useEffect(() => {
+    fetch(raw)
+      .then(r => r.text())
+      .then(text => {
+        setMarkedDown(text)
+      })
+  })
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setMarkedDown(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <section className={styles.editor}>
+        <textarea
+          type="text"
+          id='editor'
+          rows='50'
+          cols='60'
+          name='editor'
+          value={markedDown}
+          onChange={handleChange}
+        />
+      </section>
+
+      <section id='previewer' className={styles.previewer}>
+        <div dangerouslySetInnerHTML={{ __html: marked.parse(markedDown) }} />
+      </section>
     </div>
   );
 }
